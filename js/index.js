b7019505c7758a77d6ac4b275471dc764e933756
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-08-07 20:58:01 -0400

    Nav bar will enlarge and change colors

--- js/index.js
+++ js/index.js
@@ -8,5 +8,17 @@ logoImg.forEach((e) => {
     e.addEventListener('mouseleave', () => {
         e.style.transform = "scale(1)";
     });
-    
-})
+});
+
+const navBar = document.querySelectorAll('.nav-link');
+navBar.forEach((e) => {
+    e.addEventListener('mouseenter', () => {
+        e.style.transform = "scale(1.2)";
+        e.style.transition = "tranform 2s";
+        e.style.color = "blueviolet";
+    });
+    e.addEventListener('mouseleave', () => {
+        e.style.transform = "scale(1)";
+        e.style.color = "black";
+    });
+});

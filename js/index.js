6833e80858bd3d88686821f6897a18f975f9fdf5
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-08-07 20:44:25 -0400

    Added animation to the main bus pic

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+const logoImgContainer = document.querySelector('.intro');
+logoImgContainer.style.overflow = "hidden";
+const logoImg = document.querySelector('.intro img');
+logoImg.addEventListener('mouseenter', () => {
+    logoImg.style.transform = "scale(1.2)";
+    logoImg.style.transition = "transform 1s";
+});
+logoImg.addEventListener('mouseleave', () => {
+    logoImg.style.transform = "scale(1)";
+});
+
